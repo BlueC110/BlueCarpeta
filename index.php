8aad8be40fd92832b9da5dd5c1ed8fb47d2c1864
Geronimo saldaña <darcaromega@gmail.com>
2020-09-03 16:29:11 -0500

    nuevo boton del calendario

--- index.php
+++ index.php
@@ -6,18 +6,19 @@
 		<link rel="stylesheet" href="bootstrap/css/bootstrap.css">
 		<link rel="stylesheet" href="bootstrap/css/bootstrap-responsive.css">
 		<link rel="stylesheet" type="text/css" href="estilos/estilos.css">
-		<link rel="stylesheet" type="text/css" href="estilos/estilos2.css">
+		<link rel="stylesheet" type="text/css" href="css/estilos2.css">
 	<title>BlueC</title>
 </head>
-<body background="images/fondoA.jpg" style="background-attachment: fixed" >
-<div style="border: 1px solid red; height: 630px; background-image: url(images/fondoB.jpeg)"> 
+<body style="background-color: #33AAFF;">
+<div style="height: 625px; background-image: url(images/fondoB.jpeg)"> 
 
-<div style= "border: 1px solid black; width: 650px; height: 700px;">
-<p> hola como estan </p>
+<div style= "width: 645px; height: 625px; background-color: rgba(255,255,255,0.5);">
+<p style="font-size: 120px; text-align: center; padding-top: 170px; color: #2262C4; font-family:time news roman"> Bienvenidos</p>
+<p style="font-size: 120px; text-align: center; padding-top: 170px; color: #2262C4; font-family:time news roman"> a BlueC</p>
 </div>
 </div>
     <div> 
-	<center><div class="tit"><h2 style="color: #0000FF; ">Inicio de sesión</h2>
+	<center><div class="tit"><h2 style="color: white; font-family:Arial Narrow; font-size: 20pt;">Inicio de sesión</h2>
 		<center><div class="Ingreso">
 	<table border="0" align="center" valign="middle">
 		<tr>
@@ -26,10 +27,12 @@
 
 		<table border="0">
 
-		<tr><td><label style="font-size: 14pt"><b>Correo: </b></label></td>
-			<td width=80> <input class="form-group has-success" style="border-radius:15px;" type="text" name="mail"></td></tr>
-		<tr><td><label style="font-size: 14pt"><b>Contraseña: </b></label></td>
-			<td witdh=80><input style="border-radius:15px;" type="password" name="pass"></td></tr>
+		<label style="font-size: 16pt; color: white; font-family:Arial Narrow;"><b>Correo</b></label>
+			<td width=80> 
+	<input class="form-group has-s	uccess" style="border-radius:15px;" type="text" name="mail">
+	<label style="font-size: 16pt; color: white; font-family:Arial Narrow;"><b>Contraseña</b></label>
+			<td witdh=80>
+			<input style="border-radius:15px;" type="password" name="pass">
 		<tr><td></td>
 			<td width=80 align=center><input class="btn btn-primary" type="submit" value="Aceptar"></td>
 			</tr></tr></table>
@@ -39,22 +42,22 @@
 
 <form method="post" action="" >
   <fieldset>
-    <legend  style="font-size: 18pt"><b>Registro</b></legend>
+    <legend  style="font-size: 20pt; color: white; font-family:Arial Narrow;"><b>Registro</b></legend>
     <div class="form-group">
-      <label style="font-size: 14pt"><b>Nombre del estudiante</b></label>
-      <input type="text" name="realname" class="form-control" placeholder="Ingresa nombre" />
+      <label style="font-size: 16pt; color: white; font-family:Arial Narrow;"><b>Nombre del estudiante</b></label>
+      <input style="border-radius:15px;" type="text" name="realname" class="form-control"/>
     </div>
     <div class="form-group">
-      <label style="font-size: 14pt; color: #FFFFFF;"><b>ingrese email del padre del estudiante</b></label>
-      <input type="text" name="nick" class="form-control"  required placeholder="Ingresa mail"/>
+      <label style="font-size: 16pt; color: white; font-family:Arial Narrow;"><b>Ingrese email del padre del estudiante</b></label>
+      <input style="border-radius:15px;" type="text" name="nick" class="form-control"/>
     </div>
     <div class="form-group">
-      <label style="font-size: 14pt; color: #FFFFFF;"><b>ingrese tarjeta de identidad del usuario</b></label>
-      <input type="password" name="pass" class="form-control"  placeholder="Ingresa ID" />
+      <label style="font-size: 16pt; color: white; font-family:Arial Narrow;"><b>Ingrese tarjeta de identidad del usuario</b></label>
+      <input style="border-radius:15px;" type="password" name="pass" class="form-control"/>
     </div>
     <div class="form-group">
-      <label style="font-size: 14pt"><b>Repita la tarjeta de identidad</b></label>
-      <input type="password" name="rpass" class="form-control" required placeholder="repite ID" />
+      <label style="font-size: 16pt; color: white; font-family:Arial Narrow;"><b>Repita la tarjeta de identidad</b></label>
+      <input style="border-radius:15px;" type="password" name="rpass" class="form-control"/>
     </div>
 
     </div>
@@ -75,10 +78,9 @@
 		</tr>
 		</table>
 		</div></center></div></center>
-<p><a href="calendario/index.php">calendario</a></p>
+<p class="BotonCa"><a class="laA" href="calendario/index.php">calendario</a></p>
 		
-<h1>hola G hola</h1>
-<h1>Melito jeje</h1>
+
 
 
 
