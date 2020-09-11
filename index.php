50d400ab76f5e21cfd0de5ed985d399d23a5f27d
Geronimo saldaña <darcaromega@gmail.com>
2020-09-11 10:46:59 -0500

    css en archivo a parte

--- index.php
+++ index.php
@@ -5,20 +5,20 @@
 	<meta charset="utf-8">
 		<link rel="stylesheet" href="bootstrap/css/bootstrap.css">
 		<link rel="stylesheet" href="bootstrap/css/bootstrap-responsive.css">
-		<link rel="stylesheet" type="text/css" href="estilos/estilos.css">
-		<link rel="stylesheet" type="text/css" href="css/estilos2.css">
+		
+		<link rel="stylesheet" type="text/css" href="css/style1.css">
 	<title>BlueC</title>
 </head>
 <body style="background-color: #33AAFF;">
 <div style="height: 625px; background-image: url(images/fondoB.jpeg)"> 
 
-<div style= "width: 645px; height: 625px; background-color: rgba(255,255,255,0.5);">
-<p style="font-size: 120px; text-align: center; padding-top: 170px; color: #2262C4; font-family:time news roman"> Bienvenidos</p>
-<p style="font-size: 120px; text-align: center; padding-top: 170px; color: #2262C4; font-family:time news roman"> a BlueC</p>
+<div class="PrimerCont">
+<p class="PARRA"> Bienvenidos</p>
+<p class="PARRA"> a BlueC</p>
 </div>
 </div>
     <div> 
-	<center><div class="tit"><h2 style="color: white; font-family:Arial Narrow; font-size: 20pt;">Inicio de sesión</h2>
+	<center><div class="tit"><h2 class="tituloBlueC">Inicio de sesión</h2>
 		<center><div class="Ingreso">
 	<table border="0" align="center" valign="middle">
 		<tr>
@@ -27,12 +27,12 @@
 
 		<table border="0">
 
-		<label style="font-size: 16pt; color: white; font-family:Arial Narrow;"><b>Correo</b></label>
+		<label class="label30""><b>Correo</b></label>
 			<td width=80> 
-	<input class="form-group has-s	uccess" style="border-radius:15px;" type="text" name="mail">
-	<label style="font-size: 16pt; color: white; font-family:Arial Narrow;"><b>Contraseña</b></label>
+	<input class="form-group has-success" class="inputs" type="text" name="mail">
+	<label class="labels"><b>Contraseña</b></label>
 			<td witdh=80>
-			<input style="border-radius:15px;" type="password" name="pass">
+			<input class="inputs"  type="password" name="pass">
 		<tr><td></td>
 			<td width=80 align=center><input class="btn btn-primary" type="submit" value="Aceptar"></td>
 			</tr></tr></table>
@@ -42,22 +42,22 @@
 
 <form method="post" action="" >
   <fieldset>
-    <legend  style="font-size: 20pt; color: white; font-family:Arial Narrow;"><b>Registro</b></legend>
+    <legend class="legend" ><b>Registro</b></legend>
     <div class="form-group">
-      <label style="font-size: 16pt; color: white; font-family:Arial Narrow;"><b>Nombre del estudiante</b></label>
-      <input style="border-radius:15px;" type="text" name="realname" class="form-control"/>
+      <label class="labels"><b>Nombre del estudiante</b></label>
+      <input class="inputs"  type="text" name="realname" class="form-control"/>
     </div>
     <div class="form-group">
-      <label style="font-size: 16pt; color: white; font-family:Arial Narrow;"><b>Ingrese email del padre del estudiante</b></label>
-      <input style="border-radius:15px;" type="text" name="nick" class="form-control"/>
+      <label class="labels"><b>Ingrese email del padre del estudiante</b></label>
+      <input class="inputs" type="text" name="nick" class="form-control"/>
     </div>
     <div class="form-group">
-      <label style="font-size: 16pt; color: white; font-family:Arial Narrow;"><b>Ingrese tarjeta de identidad del usuario</b></label>
-      <input style="border-radius:15px;" type="password" name="pass" class="form-control"/>
+      <label class="labels"><b>Ingrese tarjeta de identidad del usuario</b></label>
+      <input class="inputs" type="password" name="pass" class="form-control"/>
     </div>
     <div class="form-group">
-      <label style="font-size: 16pt; color: white; font-family:Arial Narrow;"><b>Repita la tarjeta de identidad</b></label>
-      <input style="border-radius:15px;" type="password" name="rpass" class="form-control"/>
+      <label class="labels" ><b>Repita la tarjeta de identidad</b></label>
+      <input class="inputs" type="password" name="rpass" class="form-control"/>
     </div>
 
     </div>
