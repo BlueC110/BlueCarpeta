91e35f5c5929194639c2362da6f5096fc06ce345
Geronimo saldaña <darcaromega@gmail.com>
2020-09-21 07:52:56 -0500

    fix bugg

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 	<title>BlueC</title>
 </head>
-<<<<<<< HEAD
+
 
 <body style="background-color: #33AAFF;">
 
@@ -88,30 +88,6 @@
 
 
 
-
-=======
-<body>
-	<div> 
-	<center><div class="tit"><h2 class="tituloBlueC">Inicio de sesión</h2>
-		<center><div class="Ingreso">
-	<table border="0" align="center" valign="middle">
-		<tr>
-		<td rowspan=2>
-		<form action="validar.php" method="post">
-
-		<table border="0">
-
-		<label class="label30""><b>Correo</b></label>
-			<td width=80> 
-	<input class="form-group has-success" class="inputs" type="text" name="mail">
-	<label class="labels"><b>Contraseña</b></label>
-			<td witdh=80>
-			<input class="inputs"  type="password" name="pass">
-		<tr><td></td>
-			<td width=80 align=center><input class="btn btn-primary" type="submit" value="Aceptar"></td>
-			</tr></tr></table>
-		</form>
->>>>>>> 1b21b1f383bc4a06c1fe1b2a07819711af4dda9f
 </body>
 </html>
 
